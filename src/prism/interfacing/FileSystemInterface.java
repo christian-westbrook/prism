83b773fb3fcd1d7b07f96cf0254ec67be0429275
Christian Westbrook <christianwestbrook@live.com>
2021-02-11 23:44:52 -0700

    MVP-1 Added graph writing method to FSI

--- src/prism/interfacing/FileSystemInterface.java
+++ src/prism/interfacing/FileSystemInterface.java
@@ -1,6 +1,10 @@
 package prism.interfacing;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class FileSystemInterface
 {
@@ -12,6 +16,40 @@ public class FileSystemInterface
 
 		if(!networksDirectory.exists())
 			networksDirectory.mkdirs();
+	}
+
+	public boolean create(int[][] graph, int n, String title)
+	{
+		try
+		{
+			File newFile = new File(networksDirectory.getPath() + "\\" + title + ".data");
+
+			if(newFile.exists())
+				newFile.delete();
+
+			newFile.createNewFile();
+
+			FileWriter fw = new FileWriter(newFile);
+			BufferedWriter bw = new BufferedWriter(fw);
+			PrintWriter pw = new PrintWriter(bw);
+
+			for(int i = 0; i < n; i++)
+			{
+				for(int j = 0; j < n; j++)
+				{
+					if(graph[i][j] == 1)
+						pw.println(i + "," + j);
+				}
+			}
+
+			pw.close();
+			return true;
+		}
+		catch(IOException ex)
+		{
+			ex.printStackTrace();
+			return false;
+		}
 	}
 
 	public File getNetworksDirectory()
